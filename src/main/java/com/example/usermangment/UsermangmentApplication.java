543dcecebf423aff7b33143cf442948f11e1b8fe
Ghofrane <souayahghofrane73@gmail.com>
2026-02-02 22:18:02 +0100

    Initial commit backend spring boot

---
+++ src/main/java/com/example/usermangment/UsermangmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermangment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermangmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermangmentApplication.class, args);
+	}
+
+}
